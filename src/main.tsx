b7f5721a39da590a0696845b2fff789126eb8e75
Cjsarts0509 <4rumarts@gmail.com>
2026-02-26 08:56:42 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,9 @@
   import { createRoot } from "react-dom/client";
   import App from "./app/App.tsx";
   import "./styles/index.css";
+import "./styles/tailwind.css";
+import "./styles/theme.css";
+import "./styles/fonts.css";
 
   createRoot(document.getElementById("root")!).render(<App />);
   
