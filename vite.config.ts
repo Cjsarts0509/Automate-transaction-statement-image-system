f5dc901c521a27a9bd7930e51a440ced5d339faf
Cjsarts0509 <4rumarts@gmail.com>
2026-02-26 08:24:41 +0900

    FIX3

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // 깃허브 레포지토리 이름을 base에 반드시 설정해야 합니다. (앞뒤 슬래시 필수)
-  base: '/Contract-Creation-System/', 
+  base: '/Automate-transaction-statement-image-system/', 
 })
+
